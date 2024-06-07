c05558a51379b9b294c05070ea15ff311e07d61c
weimjay <weimjay@gmail.com>
2024-06-07 13:52:27 +1000

    css

--- app/issues/create/page.js
+++ app/issues/create/page.js
@@ -8,23 +8,37 @@ export default function NewIssuePage() {
 
   async function handleSubmit(ev) {
     ev.preventDefault();
-    const data = {title, description};
-    const res = await fetch('/api/issues', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data),
+    if (title.length < 1 || description.length < 1) {
+      alert('title or description can not be empty!');
+      return;
+    }
+    const createPromise = new Promise(async (resolve, reject) => {
+      const data = {title, description};
+      const res = await fetch('/api/issues', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+      })
+      if (res.ok) {
+        resolve();
+        setTitle('');
+        setDescription('');
+      } else {
+        reject();
+      }
     })
+
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="flex">
+      <div className="">
         <label>Title</label>
         <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} />
         <label>Description</label>
         <input type="text" value={description} onChange={ev => setDescription(ev.target.value)} />
         <div>
-          <button type="submit" className="bg-red-300 p-3">Create</button>
+          <button type="submit" className="bg-red-300 px-6 py-3 rounded-xl">Create</button>
         </div>
       </div>
     </form>
