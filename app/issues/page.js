09ad8b3bcd3b8e5ea0428c6493e6352c892cf034
weimjay <weimjay@gmail.com>
2024-06-07 14:24:27 +1000

    fix

--- app/issues/page.js
+++ app/issues/page.js
@@ -6,25 +6,29 @@ export default function IssuesPage() {
   const [issues, setIssues] = useState([]);
 
   useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  function fetchIssues() {
     fetch('/api/issues').then(response => {
       response.json().then(data => {
         setIssues(data);
       })
     })
-  }, []);
+  }
 
-  function handleDelete(id) {
-    fetch('/api/issues?_id='+id, {
+  async function handleDelete(id) {
+    await fetch('/api/issues?_id='+id, {
       'method': 'DELETE',
-      headers: {'Content-Type': 'application/json'},
     })
+    fetchIssues();
   }
 
   return (
     <section className="p-3">
       <div className="text-2xl mb-4">Issues</div>
       {issues?.length > 0 && issues.map(issue => (
-        <div className="flex gap-4 m-2">
+        <div className="flex gap-4 m-2 items-center">
           <Link href={'/issues/'+issue._id} className="text-blue-500" key={issue._id}>{issue.title}</Link>
 
           <button onClick={() => handleDelete(issue._id)} className="bg-red-300 px-3 py-1 rounded-xl">Delete</button>
