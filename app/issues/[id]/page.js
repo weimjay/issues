d74e054eee9aa7a75a7bc57ebfe8ad22c393e675
weimjay <weimjay@gmail.com>
2024-06-07 14:04:04 +1000

    handle delete

--- app/issues/[id]/page.js
+++ app/issues/[id]/page.js
@@ -1,4 +1,5 @@
 export default function DetailPage() {
+
   return (
     <section>
 
--- app/issues/page.js
+++ app/issues/page.js
@@ -3,7 +3,6 @@ import {useEffect, useState} from "react";
 import Link from "next/link";
 
 export default function IssuesPage() {
-
   const [issues, setIssues] = useState([]);
 
   useEffect(() => {
@@ -14,12 +13,21 @@ export default function IssuesPage() {
     })
   }, []);
 
+  function handleDelete(id) {
+    fetch('/api/issues?_id='+id, {
+      'method': 'DELETE',
+      headers: {'Content-Type': 'application/json'},
+    })
+  }
+
   return (
     <section className="p-3">
       <div className="text-2xl mb-4">Issues</div>
       {issues?.length > 0 && issues.map(issue => (
-        <div className="flex">
-          <Link href={'/issues/'+issue._id} className="text-blue-500 " key={issue._id}>{issue.title}</Link>
+        <div className="flex gap-4 m-2">
+          <Link href={'/issues/'+issue._id} className="text-blue-500" key={issue._id}>{issue.title}</Link>
+
+          <button onClick={() => handleDelete(issue._id)} className="bg-red-300 px-3 py-1 rounded-xl">Delete</button>
         </div>
       ))}
     </section>
